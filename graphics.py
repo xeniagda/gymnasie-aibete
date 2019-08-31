376e6c2bc28d91be32264fb174652f01d99d3ea5
admin <admin@example.com>
2019-09-01 01:56:35 +0200

    add console graphics

--- graphics.py
+++ graphics.py
@@ -1,5 +1,22 @@
+import math
+import time
 
 def drawGame(level,player):
     #level is a list of heights of pillars
     #player is a Player object
-    pass
+
+    x = round(player.x)
+    y = round(player.y)
+
+    for i in range(5):
+        line = ""
+        for j in range(len(level)):
+            if 5-i==y and j==x:
+                line += "*"
+            elif 5-i<=level[j]:
+                line += "#"
+            else:
+                line += " "
+        print(line)
+    
+    time.sleep(1)
