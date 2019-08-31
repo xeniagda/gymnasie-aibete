659075d6380d1fec6b5c677d55c0717b1a30a16c
admin <admin@example.com>
2019-09-01 01:02:07 +0200

    implement basic driver class

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from gameEngine import GameEngine
 class Driver:
     def __init__(self,level,agent):
         self.engine = GameEngine(level)
-
+        self.agent = agent
     def playGame():
-        pass
+        agentState = self.engine.getAgentState()
+        while True:
+            action = self.agent.getAction(agentState)
+            newAgentState,reward,terminate = self.engine.getNextState(action)
+
+            self.agent.update(agentState,action,newAgentState,reward)
+            if terminate:
+                break
+
