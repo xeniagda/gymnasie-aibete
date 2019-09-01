3bfd72d1fdc0766085c8601e4971bb2bd21f1b3c
Fredrik Ekholm & Jonathan Lööv <fredrik.w.ekholm@gmail.com & jonathan.loov@gmail.com>
2019-09-01 17:03:31 +0200

    Basic pygame graphics

--- graphics.py
+++ graphics.py
@@ -1,32 +1,60 @@
 import math
 import time
+import threading
+import pygame
 
-counter = 0
-def drawGame(level,player):
-    #level is a list of heights of pillars
-    #player is a Player object
-    asciiArtGraphics(level,player)
+width, height = size = 1080, 800
 
+SCALE = 50
 
-def asciiArtGraphics(level,player):
-    global counter
-    counter+=1
-    if counter<10:
-        return
-    counter = 0
-    x = int(player.x*3)
-    y = int(player.y)
+pygame.init()
 
-    print("\033[2J")
-    for i in range(10):
-        line = ""
-        for j in range(len(level)*3):
-            if 9-i==y and j==x:
-                line += "*"
-            elif 9-i<level[int(j/3)]:
-                line += "#"
-            else:
-                line += " "
-        print(line)
-    
-    time.sleep(0.02)
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("Title")
+
+pg_level = None
+pg_player = None
+
+def draw_loop():
+    global screen, pg_level, pg_player,height 
+
+    while True:
+        level = pg_level
+        player = pg_player
+        if player == None or level == None:
+            continue
+
+        print(player.y)
+        #level is a list of heights of pillars
+        #player is a Player object
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+        
+        screen.fill((255, 255, 255))
+        rec = getRect(player.x, player.y, player.width, player.height)
+        pygame.draw.rect(screen, (0, 0, 0), rec)
+
+        for x, wall_height in enumerate(level):
+            rec = getRect(x, 0, 1, wall_height)
+            pygame.draw.rect(screen, (0, 0, 0), rec)
+            
+        
+
+        pygame.display.flip()
+
+def getRect(x,y,w,h):
+    return pygame.Rect(int(x*SCALE),int(height-(y+h)*SCALE),int(w*SCALE),int(h*SCALE))
+
+def drawGame(level, player):
+    global pg_level, pg_player
+
+    pg_level = level
+    pg_player = player
+    time.sleep(0.005)
+
+thread = threading.Thread(target=draw_loop, daemon=True)
+thread.start()
