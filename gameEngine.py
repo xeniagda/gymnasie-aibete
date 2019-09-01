26f613ac4e39509890416ea80d0701f7ffb5d63f
admin <admin@example.com>
2019-09-01 10:07:24 +0200

    add simple collision detection

--- gameEngine.py
+++ gameEngine.py
@@ -1,29 +1,58 @@
 import graphics
 from util import * 
+import random
+
+random.seed(1)
 
 class GameEngine:
     def __init__(self,level=[1,1,1,2,1,1]):
         self.level = level
         self.player = Player(0,level[0],0,0)
 
-    def getNextState(self,action,draw=False,timeStep=0.1):
+    def getNextState(self,action,draw=False,timeStep=0.01):
         self.player.applyAction(action)
         self.player.move(timeStep)
-        self.resolveCollision()
+        self.resolveCollisions()
 
         if draw:
             graphics.drawGame(self.level,self.player)
         
         agentState = self.getAgentState()
         reward = 1
-        terminate = False 
+        terminate = self.player.x>=len(self.level)-1 
         return (agentState,reward,terminate)
     
     def getAgentState(self):
         return [self.player.x,self.player.y]
     
-    def resolveCollision(self):
-        pass    
+    def resolveCollisions(self):
+        self.player.isOnGround = False
+        for i in range(int(self.player.x)-1,int(self.player.x)+2):
+            if i>=0 and i<len(self.level):
+                self.player.isOnGround |= self.resolveCollisionWithBlock(i,0,1,self.level[i])
+
+    def resolveCollisionWithBlock(self,bx,by,bw,bh):
+        if(max(bx,self.player.x)<min(bx+bw,self.player.x+self.player.width)
+            and max(by,self.player.y)<min(by+bh,self.player.y+self.player.height)):
+            distLeft = self.player.x+self.player.width-bx
+            distRight = bx+bw-self.player.x
+            distDown = self.player.y+self.player.height-by
+            distUp = by+bh-self.player.y
+            minDist = min(distLeft,distRight,distUp,distDown)
+            if distUp == minDist:
+                self.player.y = by+bh
+                self.player.vy = 0
+                return True
+            if distLeft == minDist:
+                self.player.x = bx-self.player.width
+                self.player.vx = 0
+            if distRight == minDist:
+                self.player.x = bx+bw
+                self.player.vx = 0
+            if distDown == minDist:
+                self.player.y = by-self.player.height
+                self.player.vy = 0
+        return False
 
 class Player:
     def __init__(self,x,y,vx,vy):
@@ -37,16 +66,22 @@ class Player:
         self.width = 0.2
         self.height = 1
 
-        self.speed = 1
-        self.gravity = -0.2
-        self.jump = 1
+        self.groundSpeed = 1
+        self.airSpeed = 0.5
+        self.gravity = -3
+        self.jump = 4
+
+        self.isOnGround = False
     
     def applyAction(self,action):
+        currentSpeed = self.groundSpeed
+        if not self.isOnGround:
+            currentSpeed = self.airSpeed
         if action == Actions.LEFT:
-            self.vx = -self.speed
+            self.vx = -currentSpeed
         if action == Actions.RIGHT:
-            self.vx = self.speed
-        if action == Actions.JUMP and self.isOnGround():
+            self.vx = currentSpeed
+        if action == Actions.JUMP and self.isOnGround:
             self.vy = self.jump
         
     def move(self,timeStep):
