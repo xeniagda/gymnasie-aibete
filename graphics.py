c8df392bf83be46c4bde6dd69c55e50e6e40d516
admin <admin@example.com>
2019-09-01 10:06:21 +0200

    improve graphics

--- graphics.py
+++ graphics.py
@@ -1,22 +1,27 @@
 import math
 import time
 
+counter = 0
 def drawGame(level,player):
+    global counter
     #level is a list of heights of pillars
     #player is a Player object
+    counter+=1
+    if counter<10:
+        return
+    counter = 0
+    x = int(player.x*3)
+    y = int(player.y)
 
-    x = round(player.x)
-    y = round(player.y)
-
-    for i in range(5):
+    for i in range(6):
         line = ""
-        for j in range(len(level)):
+        for j in range(len(level)*3):
             if 5-i==y and j==x:
                 line += "*"
-            elif 5-i<=level[j]:
+            elif 5-i<level[int(j/3)]:
                 line += "#"
             else:
                 line += " "
         print(line)
     
-    time.sleep(1)
+    time.sleep(0.05)
