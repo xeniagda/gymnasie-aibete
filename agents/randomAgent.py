f16d37c9514ebe18257a0bbe2876d87bfaeb5a83
admin <admin@example.com>
2019-09-01 10:10:25 +0200

    add random agent

---
+++ agents/randomAgent.py
@@ -0,0 +1,14 @@
+from util import *
+import random
+
+class RandomAgent:
+    def __init__(self):
+        pass
+
+    def getAction(self,agentState):
+        if random.randint(1,10)==1:
+            return Actions.RIGHT
+        else:
+            return Actions.JUMP
+    def update(self,oldAgentState,action,newAgentState,reward):
+        pass
