47fdddcab9a8b907ff649022d71ae6f314a946e0
admin <admin@example.com>
2019-09-01 10:10:06 +0200

    fix typos

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-from gameEngine import GameEngine
-
+from gameEngine import * 
+from agents.randomAgent import *
 
 class Driver:
     def __init__(self,level,agent):
         self.engine = GameEngine(level)
         self.agent = agent
-    def playGame():
+        self.playGame()
+
+    def playGame(self):
         agentState = self.engine.getAgentState()
         while True:
             action = self.agent.getAction(agentState)
-            newAgentState,reward,terminate = self.engine.getNextState(action)
+            newAgentState,reward,terminate = self.engine.getNextState(action,True)
 
             self.agent.update(agentState,action,newAgentState,reward)
             if terminate:
                 break
 
+
+def main():
+    agent = RandomAgent()
+    driver = Driver([1,3,2,4,4,3,1,1,1,1],agent)
+
+main()
