db056d9b8a4a8733bf69a2c83633a8ff5579012e
Ivar Källström <ivar.kallstrom@gmail.com>
2019-09-07 16:37:37 +0200

    add space after ,

--- agents/randomAgent.py
+++ agents/randomAgent.py
@@ -5,10 +5,10 @@ class RandomAgent:
     def __init__(self):
         pass
 
-    def getAction(self,agentState):
-        if random.randint(1,10)==1:
+    def getAction(self, agentState):
+        if random.randint(1, 10) == 1:
             return Actions.RIGHT
         else:
             return Actions.JUMP
-    def update(self,oldAgentState,action,newAgentState,reward):
+    def update(self, oldAgentState, action, newAgentState, reward):
         pass
