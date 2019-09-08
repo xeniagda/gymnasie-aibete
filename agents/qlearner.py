1d515fb05d32b0c083263973ae43ee8f3d5faa76
loovjo <jonathan.loov@gmail.com>
2019-09-08 14:32:50 +0200

    Add q learning table

--- agents/qlearner.py
+++ agents/qlearner.py
@@ -2,6 +2,8 @@ import random
 import numpy as np
 from util import *
 
+FUTURE_DISCOUNT = 0.8 ** 0.01
+
 def agentInput2n(agentInput):
     exp_2 = 2 ** np.arange(11)
 
@@ -15,19 +17,33 @@ class Qlearner:
         # Hur ofta agenten svarar med en slumpmässig action
         self.random_epsilon = random_epsilon
 
+        self.learning_rate = 0.1
+        self.t_random = [0, None] # (time, action)
+
     def getAction(self, agentInput):
-        if random.random() < self.random_epsilon:
-            action = random.choice([Actions.LEFT, Actions.RIGHT, Actions.JUMP])
-            return action
+        if random.random() < 0.01:
+            return Actions.JUMP
+        if self.t_random[0] > 0:
+            return self.t_random[1]
         else:
             n = agentInput2n(agentInput)
-            print(n)
-
             vals = self.q_table[n]
             max_n = np.argmax(vals)
 
             return [Actions.LEFT, Actions.RIGHT, Actions.JUMP][max_n]
 
     def update(self, oldAgentInput, action, newAgentInput, reward):
+        if random.random() < self.random_epsilon:
+            self.t_random = [0.1, random.choice([Actions.LEFT, Actions.RIGHT])]
+
+        self.t_random[0] -= 0.01
+
         # Uppdatera q tabellen
-        pass
+
+        next_vals = self.q_table[agentInput2n(newAgentInput)]
+        max_q = next_vals.max()
+        q = reward + FUTURE_DISCOUNT * max_q
+
+        old_i = agentInput2n(oldAgentInput)
+        self.q_table[old_i][action.value] = \
+            self.learning_rate * q + (1 - self.learning_rate) * self.q_table[old_i][action.value]
