16ad1f501f6f288b55b8210a601371d8da9c431f
bilsen <ivar.kallstrom@gmail.com>
2019-09-21 11:27:45 +0200

    .gitignore fix

---
+++ TanktroubleML/Assets/Scripts/GameState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState
+{
+    enum Action {Left, Right, Forward, Back, Shoot};
+    
+
+
+    public GameState() {
+
+    }
+
+    // Update is called once per frame
+    void Update(Action action)
+    {
+        switch(action) {
+            case Action.Left:
+                break;
+            case Action.Right:
+                break;
+            case Action.Forward:
+                break;
+            case Action.Back:
+                break;
+            case Action.Shoot:
+                break;
+        }    
+    }
+
+
+}
