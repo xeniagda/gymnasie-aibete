1d47cdb1a2a8a8274c3215f0a63ff3dd07acb93b
Fredrik Ekholm <fredrik.w.ekholm@gmail.com>
2019-09-26 22:14:35 +0200

    improved experiment driver

--- experiment.py
+++ experiment.py
@@ -7,6 +7,9 @@ from levelGenerator import *
 import time,sys
 from gamePlayer import *
 import matplotlib.pyplot as plt
+import json
+from functools import reduce
+from operator import add
 
 RENDER = False
 
@@ -17,16 +20,19 @@ evalLevels = []
 for i in range(100):
     evalLevels.append(levelGenerator.generate(100))
 
-def evaluate(games,random_epsilon, learning_rate, future_discount):
+def evaluate(numGames,random_epsilon, learning_rate, future_discount):
     levelGenerator = IntegerLevelGenerator(0.5,0.0)
     agent = DeepQlearner(random_epsilon,future_discount,learning_rate,False)
 
     results = {
         "loss": [],
-       "reward":[]
+        "reward":[],
+        "random_epsilon":random_epsilon,
+        "learning_rate":learning_rate,
+        "future_discount":future_discount
     }
 
-    for i in range(games):
+    for i in range(numGames):
         agent.random_epsilon = random_epsilon
         playTime,avgReward = playGame(levelGenerator.generate(100),agent,2000,RENDER,ui)
 
@@ -35,11 +41,35 @@ def evaluate(games,random_epsilon, learning_rate, future_discount):
 
        # print(agent.latestLoss.numpy(),avgReward)
 
-        results["loss"].append(agent.latestLoss.numpy())
+        results["loss"].append(agent.latestLoss.numpy().item())
         results["reward"].append(avgReward)
-        print(str(i) + "/"+str(games))
+       # print(str(i) + "/"+str(numGames))
 
     return results
+
+def averageLists(lists):
+    avg = list(reduce(lambda results1,results2: map(add,results1,results2),lists))
+    avg = [s/len(lists) for s in avg]
+    return avg
+
+
+def evaluateManyTimes(numTimes,numGames,random_epsilon, learning_rate, future_discount):
+    totResults = {
+        "loss": [],
+        "reward":[],
+        "random_epsilon":random_epsilon,
+        "learning_rate":learning_rate,
+        "future_discount":future_discount
+    }
+    for i in range(numTimes):
+        partialResult = evaluate(numGames,random_epsilon,learning_rate,future_discount)
+        totResults["loss"].append(partialResult["loss"])
+        totResults["reward"].append(partialResult["reward"])
+        print(i+1,random_epsilon)
+    
+    totResults["loss"] = averageLists(totResults["loss"])
+    totResults["reward"] = averageLists(totResults["reward"])
+    return totResults
 
 def averageChunks(vals,chunkSize):
     res = []
@@ -47,33 +77,42 @@ def averageChunks(vals,chunkSize):
         res.append(sum(vals[i:i+chunkSize])/chunkSize)
     return res
 
-def main():
-    
-    allResults = []
+def saveResults(resultsList):
+    jsonData = json.dumps(resultsList)
 
-    allResults.append(evaluate(100,0.05,0.0025,0.8))
-    allResults.append(evaluate(100,0.05,0.005,0.8))
-    allResults.append(evaluate(100,0.05,0.01,0.8))
-    
-    for results in allResults:
-        print(results["loss"])
-        print(results["reward"])
+    f = open("latestResults.jsonData","w")
+    f.write(jsonData)
+    f.close()
 
-        
+
+def showResult(resultsList):
+    saveResults(resultsList)
+
     plt.subplot(1,2,1)
-    for i in range(len(allResults)):
-        plt.plot(averageChunks(allResults[i]["loss"],10),label=str(i))
+    for results in resultsList:
+        plt.plot(averageChunks(results["loss"],10),label=("η="+str(results["learning_rate"])))
 
     plt.title('Loss')
+    plt.yscale('log')
     plt.legend(loc='upper left')
 
     plt.subplot(1,2,2)
-    for i in range(len(allResults)):
-        plt.plot(averageChunks(allResults[i]["reward"],10),label=str(i))
+    for results in resultsList:
+        plt.plot(averageChunks(results["reward"],10),label=("η="+str(results["learning_rate"])))
 
     plt.title('Reward')
     plt.legend(loc='upper left')
     plt.show()
+
+def main():
+    
+    resultsList = []
+
+    resultsList.append(evaluateManyTimes(20,200,0.05,0.0025,0.8))
+    resultsList.append(evaluateManyTimes(20,200,0.05,0.005,0.8))
+    resultsList.append(evaluateManyTimes(20,200,0.05,0.01,0.8))
+
+    showResult(resultsList)
 
 if RENDER:
     threading.Thread(target=main, daemon=True).start()
