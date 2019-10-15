5494da9f7a424e5003b59e9dbabe7b336d3393c0
Ivar Källström <ivar.kallstrom@gmail.com>
2019-10-15 14:26:53 +0200

    stuff

--- experiment/experimentLayout.py
+++ experiment/experimentLayout.py
@@ -2,25 +2,22 @@
 
 {
     "testExperiment": {
+        
 
+        "numLevels": 1,
+        "ticksPerLevel": 12412343,
         "runsPerSet": 4,
         "ticks": 100,
         "parameterSets": [
             {
                 "parameters": {
-                    "learningRate": 0,
-                    "futureDiscount": 0,
-                    "randomActionMethod": Trandom(0.6,1/6),
-                    "runs": 4,
-                },
-                "results": [
-                    {
-                        "loss":[],
-                        "reward":[]
-                    }
-                ]
+                    "learningRate": lambda t: 0,
+                    "futureDiscount": lambda t: 0,
+                    "randomActionMethod": lambda t: TRandom(0.6*t,1/6),
+                }
             }
         ],
     }
 
 }
+
--- experiment/experimentNew.py
+++ experiment/experimentNew.py
@@ -1,14 +1,53 @@
 
 
+from ParameterSet import ParameterSet
+sys.path.insert(0, "..")
+import levelGenerator
+import json
+
 class Experiment:
 
-    def __init__(self):
         
-        pass
+    def __init__(self, **kwargs):
+        self.setup(kwargs)
+        self.settings = kwargs
 
 
-    def load(data):
+    def setup(
+            self,
+            *parameterSets, 
+            name="experiment", 
+            runsPerSet=1, 
+            numLevels=1, 
+            ticksPerLevel=12313, 
+            levelGenerator = None, 
+            agentType="dq", 
+            **kwargs):
+        self.parameterSets = [ParameterSet.loadFromDict(paramSet) for paramSet in parameterSets]
+        self.runsPerSet = runsPerSet
+        self.ticksPerLevel = ticksPerLevel
+        self.numLevels = numLevels
+        self.levelGenerator = levelGenerator
+        self.agentType = agentType
         
-        pass
 
-            
+
+    @staticmethod
+    def loadFromDict(data):
+        return Experiment(**data)
+
+    def saveToFile(self, path):
+        with open(path, "w") as f:            
+            f.write(json.dumps({
+                "settings": self.settings, 
+                "parameterSets": [paramSet.dictify() for paramSet in self.parameterSets]
+            }))
+        
+
+
+    def run(self):
+        for paramSet in self.parameterSets:
+            run(paramSet, self.levelGenerator, self.ticksPerLevel, self.numLevels, self.agentType) 
+
+    def plot(self, plt):
+        self.parameterSets
