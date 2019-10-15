26bd24eb905f7d55ec72bd44b75f96740943f16b
Ivar Källström <ivar.kallstrom@gmail.com>
2019-10-15 12:06:59 +0200

    begun experiment restucturing

---
+++ experiment/experimentLayout.py
@@ -0,0 +1,26 @@
+
+
+{
+    "testExperiment": {
+
+        "runsPerSet": 4,
+        "ticks": 100,
+        "parameterSets": [
+            {
+                "parameters": {
+                    "learningRate": 0,
+                    "futureDiscount": 0,
+                    "randomActionMethod": Trandom(0.6,1/6),
+                    "runs": 4,
+                },
+                "results": [
+                    {
+                        "loss":[],
+                        "reward":[]
+                    }
+                ]
+            }
+        ],
+    }
+
+}
