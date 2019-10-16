f8c1b35021e4eb91a026bcc55f7524a988f9cbc0
Fredrik Ekholm <fredrik.w.ekholm@gmail.com>
2019-10-16 17:54:58 +0200

    add plotter

---
+++ experiment/plotter.py
@@ -0,0 +1,73 @@
+import matplotlib.pyplot as plt
+from functools import reduce
+from operator import add
+import math,time
+
+DESIRED_DATA_POINTS = 20
+
+def averageLists(lists):
+    if type(lists[0])!=list:
+        return lists
+    avg = list(reduce(lambda results1,results2: map(add,results1,results2),lists))
+    avg = [s/len(lists) for s in avg]
+    return avg
+
+def zipDicts(d):
+    ret = {}
+
+    for res in d:
+        for key, value in res.items():
+            if key in ret:
+                ret[key].append(value)
+            else:
+                ret[key] = [value]
+    
+    return ret
+
+def averageChunks(vals,chunkSize):
+    res = []
+    for i in range(0, len(vals), chunkSize):
+        res.append(sum(vals[i:i+chunkSize])/chunkSize)
+    return res
+
+class Plotter():
+    def __init__(self):
+        pass
+    
+    def plotExperiment(experimentData,plotOnlyAverage=True):
+
+        fig, (lossSubplot,rewardSubplot) = plt.subplots(1,2)
+        fig.set_size_inches(18.5, 10.5)
+
+        lossSubplot.set_title('Loss')
+        lossSubplot.set_yscale('log')
+        lossSubplot.legend(loc='upper left')
+        rewardSubplot.set_title('Reward')
+        rewardSubplot.legend(loc='upper left')
+
+        for i,parameterSetData in enumerate(experimentData["parameterSets"]):
+            Plotter.addParemterSetToSubplots(parameterSetData,{
+                "loss":lossSubplot,
+                "reward":rewardSubplot
+            },i,plotOnlyAverage)
+
+        fig.show()
+
+    def addParemterSetToSubplots(parameterSetData,subplots,colorIndex,plotOnlyAverage):
+        label = "LR=" + parameterSetData["learningRate"]
+        label += "FD=" + parameterSetData["futureDiscount"]
+        label += "RAM=" + parameterSetData["randomActionMethod"]
+
+        zippedResults = zipDicts(parameterSetData["results"])
+        for key,value in zippedResults.items():
+            if not plotOnlyAverage:
+                Plotter.addCurvesToSubplot(subplots[key],value,label,colorIndex)
+            
+            Plotter.addAverageToSubplot(subplots[key],value,label,colorIndex)
+
+    def addCurvesToSubplot(subplot,dataLists,label,colorIndex):
+        for d in dataLists:
+            subplot.plot(d,color=("C"+str(colorIndex)),alpha=0.2)
+
+    def addAverageToSubplot(subplot,dataLists,label,colorIndex):
+        subplot.plot(averageLists(dataLists),color=("C"+str(colorIndex)),label=label)
