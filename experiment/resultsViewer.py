e87db8709628120d5e5092fbe941c79dabab41e1
Fredrik Ekholm <fredrik.w.ekholm@gmail.com>
2019-10-19 22:56:27 +0200

    add modification date to results picker

--- experiment/resultsViewer.py
+++ experiment/resultsViewer.py
@@ -1,8 +1,10 @@
 import os,json
 from plotter import *
+from datetime import datetime
 
-def getExperimentNames():
-    return list(map(lambda name: name[:-5],os.listdir("results/data")))
+def getExperimentFiles():
+    fileNames = list(map(lambda name: name[:-5],os.listdir("results/data")))
+    return sorted([(os.path.getmtime("results/data/"+name+".json"),name) for name in fileNames])
 
 def loadResults(saveName):
     with open("results/data/" + saveName + ".json","r") as f:
@@ -10,10 +12,10 @@ def loadResults(saveName):
 
 def main():
 
-    experimentNames = getExperimentNames()
+    experimentNames = getExperimentFiles()
     while True:
-        for i,experimentName in enumerate(experimentNames):
-            print(str(i)+": "+experimentName)
+        for i,(modifiedTimestamp, experimentName) in enumerate(experimentNames):
+            print(str(i)+": "+experimentName, "("+datetime.utcfromtimestamp(modifiedTimestamp).strftime('%Y-%m-%d %H:%M')+")")
         print(str(len(experimentNames))+": exit")
 
         choice = int(input("Your choice:"))
@@ -21,6 +23,6 @@ def main():
         if choice==len(experimentNames):
             return
 
-        Plotter.plotExperiment(loadResults(experimentNames[choice]))
+        Plotter.plotExperiment(loadResults(experimentNames[choice][1]))
 
 main()
