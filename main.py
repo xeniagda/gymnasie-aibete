1de5fd90a5360dffea5956ffce95a47ffb7aa4e4
loovjo <jonathan.loov@gmail.com>
2019-10-28 21:03:10 +0100

    Make main use gamePlayer again

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ from random_action_method import *
 import levelGenerator
 import time,sys
 from gamePlayer import *
-from multiGameEngine import MultiGameEngine
-from time import sleep
+
 
 RANDOM_EPSILON = 0.2
 RENDER = True
@@ -21,63 +20,35 @@ MAX_TIME = 400
 
 totalTicks = 0
 
-# class Driver:
-#     def __init__(self,level_size,agent):
-#         self.level_size = level_size
-#         self.agent = agent
-#         self.rewards = []
+N_GAMES = 100
 
-#         self.startTime = time.time()
-#         self.numTicks = 0
+class Driver:
+    def __init__(self,level_size,agent):
+        self.level_size = level_size
+        self.agent = agent
+        self.rewards = []
 
-#     def play(self):
-#         global totalTicks
-#         while True:
-#             gamelen,reward = playGame(WORLD_TYPE.generate(self.level_size),self.agent,MAX_TIME,RENDER,ui)
-#             totalTicks += gamelen
-#             if totalTicks%1000==0:
-#                 print(totalTicks,round(reward))
+        self.startTime = time.time()
+        self.numTicks = 0
 
-N_GAMES = 1 # Kör och växla mellan flera spel, mest för att se att inte MultiGameEngine är trasig
+    def play(self):
+        global totalTicks
+        while True:
+            levels = [WORLD_TYPE.generate(self.level_size) for _ in range(N_GAMES)]
+            gamelen,reward = playGames(levels,self.agent,MAX_TIME,RENDER,ui)
+            totalTicks += gamelen
+            if totalTicks%1000==0:
+                print(totalTicks,round(reward))
 
 ui = UI(RENDER,0.01)
 
 def main():
-    # agent = DuelingDQL(TRandom(RANDOM_EPSILON, 1/6), future_discount=0.8, learning_rate=0.04,saveAndLoad=True)
-    agent = HumanAgent(ui)
+    agent = DeepQlearner(TRandom(RANDOM_EPSILON, 1/6), future_discount=0.8, learning_rate=0.04,saveAndLoad=True)
+    # agent = HumanAgent(ui)
 
-    # driver = Driver(WORLD_SIZE, agent)
+    driver = Driver(WORLD_SIZE, agent)
     
-    # driver.play()
-
-    timer = 0
-
-    levels = [WORLD_TYPE.generate(20) for n in range(N_GAMES)]
-    shortest = min(map(len, levels))
-    levels = [level[:shortest] for level in levels]
-
-    curr_game = 0
-    ges = MultiGameEngine(levels)
-
-    ui.setAgent(agent)
-
-    while True:
-        agentInput = None
-        action = agent.getAction(agentInput)
-
-        ai, rew = ges.performTick([Actions.NONE] * curr_game + [action] + [Actions.NONE] * (N_GAMES - curr_game - 1))
-
-        ui.setAgentInput(ai[curr_game])
-        ui.setReward(rew[curr_game])
-        ge = ges.into_regular_engine(curr_game, ui)
-        ui.setGameEngine(ge)
-
-        sleep(ui.sleepTime)
-
-        timer += 1
-        if timer > 300:
-            timer = 0
-            curr_game = (curr_game + 1) % N_GAMES
+    driver.play()
 
 
 if RENDER:
